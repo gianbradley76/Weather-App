023e1cd4570a98b88763d9945ddd2a87e2a3cb8a
Gian <gbpmendoza76@gmail.com>
2021-12-19 07:08:05 +0800

    Fixed API calls. Added basic hourly temperature data

--- src/fetch.js
+++ src/fetch.js
@@ -1,12 +1,29 @@
 async function fetchData(city) {
 	const API_KEY = '3d17cb75b5ada3fe1c289e344222ddea';
-	const response = await fetch(
-		`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
+
+	// Initial fetch request for the city
+	const response = await fetchAPI(city, API_KEY);
+
+	const latitude = response.coord.lat;
+	const longitude = response.coord.lon;
+	// Fetches API that will be used
+	const weatherFetch = await fetch(
+		`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`,
 		{ mode: 'cors' }
 	);
-	const weatherData = await response.json();
-	console.log(weatherData);
+	const weatherData = await weatherFetch.json();
+
 	return weatherData;
+}
+
+// Fetches API to get longitude and latitude
+async function fetchAPI(city, API_KEY) {
+	const initialFetch = await fetch(
+		`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`,
+		{ mode: 'cors' }
+	);
+	const initialAPI = await initialFetch.json();
+	return initialAPI;
 }
 
 export { fetchData };
--- src/index.js
+++ src/index.js
@@ -2,38 +2,41 @@ import { fetchData } from './fetch';
 
 // window.addEventListener('DOMContentLoaded', displayWeather('Manila'));
 
-function displayWeather(city) {
-	fetchData(city).then((data) => {
-		showTemp(data.main.temp);
-		showMainWeather(data.weather[0].main);
+function displayWeather(data) {
+	showTemp(data.current.temp);
+	showMainWeather(data.current.weather[0].main);
 
-		const dataIcon = data.weather[0].icon;
-		const weatherImg = document.getElementById('weather-img');
+	const dataIcon = data.current.weather[0].icon;
+	const weatherImg = document.getElementById('weatherIcon');
 
-		weatherImg.src = `http://openweathermap.org/img/wn/${dataIcon}.png`;
-	});
+	weatherImg.src = `http://openweathermap.org/img/wn/${dataIcon}.png`;
+
+	console.log(data.hourly);
+	displayHourlyInfo(data.hourly);
+}
+
+function displayHourlyInfo(hourlyData) {
+	for (let i = 0; i < 5; i++) {
+		document.querySelector(`.hour-${i}`).innerText = hourlyData[i].feels_like;
+	}
 }
 
 // Selecting DOMs for events
-const form = document.querySelector('.city-form');
+const submitCity = document.getElementById('submitCity');
 
 function showTemp(temp) {
-	const currTemp = document.getElementById('curr-temp');
+	const currTemp = document.getElementById('currTemp');
 	currTemp.innerText = temp;
 }
 
 function showMainWeather(mainWeatherData) {
-	const mainWeather = document.getElementById('main-weather');
+	const mainWeather = document.getElementById('mainWeather');
 	mainWeather.innerText = mainWeatherData;
 }
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	const cityInput = document.getElementById('city-input').value;
-	fetchData(cityInput).then((data) => {
-		showTemp(data.main.temp);
-		showMainWeather(data.weather[0].main);
-	});
-	form.reset();
+submitCity.addEventListener('click', async () => {
+	const cityInput = document.getElementById('cityInput').value;
+	const data = await fetchData(cityInput);
+	displayWeather(data);
+	console.log('final: ', data);
 });
